783ad4ea93132a948d8af883ffaea22305641600
Nikolay <paravoz_2014@mail.ru>
2021-05-11 22:25:52 +0300

    second page v 0.1

--- dist/js/main.js
+++ dist/js/main.js
@@ -35,6 +35,24 @@
     })
   })();
  
+// video
+$(function () {
+	var $videoContainer = $('#video'),
+		$videoControls = $('.video-control'),
+		$video = $('#myVideo')[0];
+
+	$videoControls.click(function () {
+		if ($video.paused) {
+			$video.play();
+			$videoContainer.addClass('video-is-playing');
+		} else {
+			$video.pause();
+			$videoContainer.removeClass('video-is-playing');
+			//	возвращаем постер
+			$video.load();
+		}
+	});
+});
 
 
 // slider swiper
