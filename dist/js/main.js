5d68de48e5f6bfa592d9bfdab1ff0e0c5c332e6f
Nikolay <paravoz_2014@mail.ru>
2021-06-02 19:49:00 +0300

    final v.1.1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,72 +1,123 @@
-  // function for webp work
-  function testWebP(callback) {
-
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
+// function for webp work
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
     callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
-    if (support == true) {
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
     document.querySelector('body').classList.add('webp');
-    }else{
+  } else {
     document.querySelector('body').classList.add('no-webp');
-    }
-    });
+  }
+});
 
+(function () {
+  var buttonMenu = document.querySelector('.btn-menu')
 
-
-
- // btn menu 
-
-  (function () {
-    var buttonMenu = document.querySelector('.btn-menu')
-  
+  if (buttonMenu) {
     buttonMenu.addEventListener('click', function () {
       var menu = document.querySelector('.rightside-menu')
       menu.classList.remove('rightside-menu-close')
     })
-  
-    var rightsideMenuButton = document.querySelector('.rightside-menu-btn')
-  
+  }
+
+  var rightsideMenuButton = document.querySelector('.rightside-menu-btn')
+
+  if (rightsideMenuButton) {
     rightsideMenuButton.addEventListener('click', function () {
       var menu = document.querySelector('.rightside-menu')
       menu.classList.add('rightside-menu-close')
     })
-  })();
+  }
+})();
 
-  // adaptiv menu burger
-
-
-  $(document).ready(function() {
-    $('.hamburger').click(function(event) {
-      $('.hamburger, nav').toggleClass('active');      
-    });    
+$(document).ready(function () {
+  $('.hamburger').click(function (event) {
+    $('.hamburger, nav').toggleClass('active');
   });
 
-  // mixitUp for gallery
-  
-   var mixer = mixitup('.container-gallery',{
-    load: {
-      filter: '.living'
+  var $videoContainer = $('#video'),
+    $videoControls = $('.video-control'),
+    $video = $('#myVideo')[0];
+
+  $videoControls.click(function () {
+    $videoControls.toggleClass('pause', $video.paused);
+    $videoContainer.toggleClass('video-is-playing', !$video.paused);
+
+    if ($video.paused) {
+      $video.play();
+    } else {
+      $video.pause();
+      $video.load();
     }
   });
 
-
-     // slider swiper
-
   var swiper1 = new Swiper('.swiper1', {
-    // Optional parameters
     slidesPerView: 1,
     autoHeight: true,
     loop: true,
     freeMode: true,
-    pagination: {        
-        el: '.swiper-pagination',
-        clickable: true,
-        
-      },   
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
   });
+  
+  var swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    // slidesPerView: 10,
+    spaceBetween: 10,
+    slidesPerGroup: 10,
+    loop: true,
+    freeMode: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 10,
+      },
+  
+      800: {
+        slidesPerView: 7,
+      },
+  
+      640: {
+        slidesPerView: 5,
+      },
+  
+      400: {
+        slidesPerView: 3,
+      },
+  
+      300: {
+        slidesPerView: 2,
+      },
+    },
+  });
+  
+  var swiper3 = new Swiper('.swiper3', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+  });
+
+  var galleryContainers = document.querySelectorAll('.container-gallery');
+
+  if (galleryContainers.length) {
+    var mixer = mixitup(galleryContainers, {
+      load: {
+        filter: '.living'
+      }
+    });
+  }
+});
